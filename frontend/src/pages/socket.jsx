bc36ee0896f74104cbd54034145b5da662a69c4d
umar-md <20r21a12f5@mlrinstitutions.ac.in>
2026-05-07 11:20:08 +0530

    web sockets

--- frontend/src/pages/socket.jsx
+++ frontend/src/pages/socket.jsx
@@ -4,6 +4,12 @@ import { backendURI } from "../mainApi";
 const socket = io(backendURI, {
   autoConnect: false,
   withCredentials: true,
+  transports: ["websocket", "polling"],
+  upgrade: true,
+  reconnection: true,
+  reconnectionAttempts: 10,
+  reconnectionDelay: 1000,
+  timeout: 20000,
 });
 
 export default socket;
